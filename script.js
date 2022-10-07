8d7140e2cacfbc28a4c77b43f7b578669dc6a9a7
Elizabeth Kennedy <eakennedy.2412@gmail.com>
2022-10-07 00:20:03 -0700

    set up to fetch weather data

---
+++ script.js
@@ -0,0 +1,93 @@
+let weather = {
+    cur: 0,
+    mileOrMeter: ["mph", "m/s"],
+    celOrFah: ["F", "C"],
+    unit: ["imperial", "metric"],
+    apiKey: "2a880f92df617e69a1d7cb92c3da66cf",
+    fetchWeather: function (city) {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${
+          this.unit[this.cur]
+        }&appid=${this.apiKey}`
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { country } = data.sys;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity, temp_min, temp_max, feels_like } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText =
+        "Weather in " + name + " " + country;
+      document.querySelector(".temp").innerText = temp + "°";
+  
+      document.querySelector(".unitFahrenheit ").innerText = "°F";
+      document.querySelector(".or").innerHTML = "|";
+      document.querySelector(".unitCelsius").innerText = "°C";
+  
+      document.querySelector(".icon").src ="https://openweathermap.org/img/wn/" + icon + ".png";
+  
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".feels").innerText =
+        "Low: " +
+        temp_min +
+        " °" +
+        this.celOrFah[this.cur] +
+        "\n High: " +
+        temp_max +
+        " °" +
+        this.celOrFah[this.cur] +
+        "\n Feels like: " +
+        feels_like +
+        " °" +
+        this.celOrFah[this.cur];
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " " + this.mileOrMeter[this.cur];
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage ="(url = 'https://imageshack.com/i/pnpHIqG9j')"
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document
+    .querySelector(".unitFahrenheit")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(".card").style.color = "blanchedalmond";
+      weather.cur = 0;
+      weather.fetchWeather(document.querySelector(".search-bar").value);
+    });
+  document.querySelector(".unitCelsius").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(".card").style.color = "aqua";
+    weather.cur = 1;
+    weather.fetchWeather(document.querySelector(".search-bar").value);
+  });
+  
+  document
+    .querySelector(".search > button")
+    .addEventListener("click", function () {
+      weather.search();
+    });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  weather.fetchWeather("Marina del Rey");
